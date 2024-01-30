63b58e14d3b8053a3acc9d0aba78dc0737410be1
Garrett Kinder <garrettkinder92@gmail.com>
2024-01-30 16:31:48 -0500

    so many edits made!!!!

--- Functions.js
+++ Functions.js
@@ -9,7 +9,7 @@ async function load_file(url) {
 }
 
 const file_url = "data.js";
-const file_content = load_file(file_url);
+//onst file_content = load_file(file_url);
 
 async function pet_card() {
     try {
@@ -50,64 +50,63 @@ async function pet_card() {
     }
 }
 
+function getQueryParam(key){
+    let queryParams=new URLSearchParams(window.location.search);
+    return queryParams.has(key) ? queryParams.get(key) : null;
+}
 
-
-// async function pet_details() {
-//     try {
+function pet_details(id) {
+        if (id !== null && typeof pets !== 'undefined' && id >= 0 && id < pets.length) {
+            // const pet = pets[index];
+            let html = `
+            <div class="row">
+    <div class="col-lg-6 col-sm-12">
+        <img src="${pets[id].image}" class="card-img-top" alt="Image of a ${pets[id].breed} ${pets[id].type} on the grass">
+    </div>
+    <div class="col-lg-6 col-sm-12">
+        <dl class="row" id="pet_box">
+        </dl>
     
-//         let html = '';
+        <dt class="col-sm-3">Type:</dt>
+        <dd class="col-sm-9">${pets[id].type}</dd>
         
-//        for (let i=0; i < pets.length; i++){
-//         let htmlSegment = `<div class="row">
-//         <div class="col-lg-6 col-sm-12">
-//           <img src="${pets[i].image}" class="card-img-top" alt="Image of a ${pets[i].breed} ${pets[i].type} on the grass">
-//         </div>
-//         <div class="col-lg-6 col-sm-12">
-//           <dl class="row" id="pet_box">
-//           </dl>
-//         </div>
-//             </div>
-//             <dt class="col-sm-3">Type:</dt>
-//             <dd class="col-sm-9">${pets[i].type}</dd>
-          
-//             <dt class="col-sm-3">Breed:</dt>
-//             <dd class="col-sm-9">${pets[i].breed}</dd>
-          
-//             <dt class="col-sm-3">Sex:</dt>
-//             <dd class="col-sm-9">${pets[i].sex}</dd>
-          
-//             <dt class="col-sm-3">Color:</dt>
-//             <dd class="col-sm-9">${pets[i].color}</dd>
-          
-//             <dt class="col-sm-3">Spayed / Neutered:</dt>
-//             <dd class="col-sm-9">${pets[i].fixed}</dd>
+        <dt class="col-sm-3">Breed:</dt>
+        <dd class="col-sm-9">${pets[id].breed}</dd>
+        
+        <dt class="col-sm-3">Sex:</dt>
+        <dd class="col-sm-9">${pets[id].sex}</dd>
+        
+        <dt class="col-sm-3">Color:</dt>
+        <dd class="col-sm-9">${pets[id].color}</dd>
+        
+        <dt class="col-sm-3">Spayed / Neutered:</dt>
+        <dd class="col-sm-9">${pets[id].fixed}</dd>
 
-//             <dt class="col-sm-3">Age:</dt>
-//             <dd class="col-sm-9">${pets[i].age}</dd>
+        <dt class="col-sm-3">Age:</dt>
+        <dd class="col-sm-9">${pets[id].age}</dd>
 
-//             <dt class="col-sm-3">Animal ID:</dt>
-//             <dd class="col-sm-9">${pets[i].animal_id}</dd>
+        <dt class="col-sm-3">Animal ID:</dt>
+        <dd class="col-sm-9">${pets[id].animal_id}</dd>
 
-//             <dt class="col-sm-3">Microchip Number:</dt>
-//             <dd class="col-sm-9">${pets[i].microchip_num}</dd>
+        <dt class="col-sm-3">Microchip Number:</dt>
+        <dd class="col-sm-9">${pets[id].microchip_num}</dd>
 
-//             <dt class="col-sm-3">Status:</dt>
-//             <dd class="col-sm-9">${pets[i].status}</dd>
+        <dt class="col-sm-3">Status:</dt>
+        <dd class="col-sm-9">${pets[id].status[0]}</dd>
+        <dd class="col-sm-9">${pets[id].status[1]}</dd>
+        <dd class="col-sm-9">${pets[id].status[2]}</dd>
+        <dd class="col-sm-9">${pets[id].status[3]}</dd>
 
-//             <dt class="col-sm-3">A Little Bit About Me:</dt>
-//             <dd class="col-sm-9">
-//             ${pets[i].about_me}
-//             </dd>`;
-//             html += htmlSegment;
-//        };
-//        document.getElementById("pet_details_container").innerHTML = html;
-//     } catch (error) {
-//         console.error("Error while processing pet data:", error);
-//     }
-// }
-
-document.addEventListener("DOMContentLoaded", function() {
-    pet_card() });
-
-document.addEventListener("DOMContentLoaded", function() {
-    pet_details() });
+        <dt class="col-sm-3">A Little Bit About Me:</dt>
+        <dd class="col-sm-9">
+        ${pets[id].about_me}
+        </dd>
+        </div>
+        </div>
+            `;
+            document.getElementById('pet_details_container').innerHTML = html;
+        } else {
+           window.location.href = 'index.html';
+        }
+    }
+;
