f52c616227758505df63da41ada7250d6d6b67e0
Garrett Kinder <garrettkinder92@gmail.com>
2024-01-29 21:54:02 -0500

    function b.s.

--- Functions.js
+++ Functions.js
@@ -1,14 +1,43 @@
-<script src="data.js"></script>
-
-const pet_data = "data.js";
-
-function pet_cards(){
-    let contentDiv=document.getElementsByClassName("col-lg-4 col-sm-12");
-    for(let i = 0; i<3; i++) {
-        contentDiv.innerHTML+=`
-        <div>
-            <h2>${pets[i].author}</h2>
-            <p>${pets[i].text}</p>
-        </div>`;
+async function load_file(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+        return data;
+    }   catch (error) {
+        console.error("Error while loading the file:", error);
     }
 }
+
+const file_url = "data.js";
+const file_content = await load_file(file_url);
+
+async function pet_card() {
+    try {
+        const results = JSON.parse(await load_file(file_url));
+        console.log(results);
+
+        let html = '';
+
+        results.forEach(pet => {
+            let htmlSegment = `
+                <div class="col-lg-4 col-sm-12">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${pet.image}" class="card-img-top" alt="Image of ${pet.name}">
+                        <div class="card-body">
+                            <h5 class="card-title">${pet.name}</h5>
+                            <a href="detail.html" class="btn btn-primary">Details on ${pet.name}...</a>
+                        </div>
+                    </div>
+                </div>`;
+                html += htmlSegment;
+        });
+
+        document.getElementById("pet_container").innerHTML = html;
+    } catch (error) {
+        console.error("Error while processing pet data:", error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded, function() {
+    pet_card();
+});
