a0e681ab01af4c8fd666cd5b7eca815be5aa796f
Isha Nepal <ishanepal198@gmail.com>
2024-01-30 14:57:05 -0500

    Details edited

--- Functions.js
+++ Functions.js
@@ -50,59 +50,61 @@ async function pet_card() {
     }
 }
 
-async function pet_details() {
-    try {
+
+
+// async function pet_details() {
+//     try {
+    
+//         let html = '';
         
-        let html = '';
-        
-       for (let i=0; i < pets.length; i++){
-        let htmlSegment = `<div class="row">
-        <div class="col-lg-6 col-sm-12">
-          <img src="${pets[i].image}" class="card-img-top" alt="Image of a ${pets[i].breed} ${pets[i].type} on the grass">
-        </div>
-        <div class="col-lg-6 col-sm-12">
-          <dl class="row" id="pet_box">
-          </dl>
-        </div>
-            </div>
-            <dt class="col-sm-3">Type:</dt>
-            <dd class="col-sm-9">${pets[i].type}</dd>
+//        for (let i=0; i < pets.length; i++){
+//         let htmlSegment = `<div class="row">
+//         <div class="col-lg-6 col-sm-12">
+//           <img src="${pets[i].image}" class="card-img-top" alt="Image of a ${pets[i].breed} ${pets[i].type} on the grass">
+//         </div>
+//         <div class="col-lg-6 col-sm-12">
+//           <dl class="row" id="pet_box">
+//           </dl>
+//         </div>
+//             </div>
+//             <dt class="col-sm-3">Type:</dt>
+//             <dd class="col-sm-9">${pets[i].type}</dd>
           
-            <dt class="col-sm-3">Breed:</dt>
-            <dd class="col-sm-9">${pets[i].breed}</dd>
+//             <dt class="col-sm-3">Breed:</dt>
+//             <dd class="col-sm-9">${pets[i].breed}</dd>
           
-            <dt class="col-sm-3">Sex:</dt>
-            <dd class="col-sm-9">${pets[i].sex}</dd>
+//             <dt class="col-sm-3">Sex:</dt>
+//             <dd class="col-sm-9">${pets[i].sex}</dd>
           
-            <dt class="col-sm-3">Color:</dt>
-            <dd class="col-sm-9">${pets[i].color}</dd>
+//             <dt class="col-sm-3">Color:</dt>
+//             <dd class="col-sm-9">${pets[i].color}</dd>
           
-            <dt class="col-sm-3">Spayed / Neutered:</dt>
-            <dd class="col-sm-9">${pets[i].fixed}</dd>
+//             <dt class="col-sm-3">Spayed / Neutered:</dt>
+//             <dd class="col-sm-9">${pets[i].fixed}</dd>
 
-            <dt class="col-sm-3">Age:</dt>
-            <dd class="col-sm-9">${pets[i].age}</dd>
+//             <dt class="col-sm-3">Age:</dt>
+//             <dd class="col-sm-9">${pets[i].age}</dd>
 
-            <dt class="col-sm-3">Animal ID:</dt>
-            <dd class="col-sm-9">${pets[i].animal_id}</dd>
+//             <dt class="col-sm-3">Animal ID:</dt>
+//             <dd class="col-sm-9">${pets[i].animal_id}</dd>
 
-            <dt class="col-sm-3">Microchip Number:</dt>
-            <dd class="col-sm-9">${pets[i].microchip_num}</dd>
+//             <dt class="col-sm-3">Microchip Number:</dt>
+//             <dd class="col-sm-9">${pets[i].microchip_num}</dd>
 
-            <dt class="col-sm-3">Status:</dt>
-            <dd class="col-sm-9">${pets[i].status}</dd>
+//             <dt class="col-sm-3">Status:</dt>
+//             <dd class="col-sm-9">${pets[i].status}</dd>
 
-            <dt class="col-sm-3">A Little Bit About Me:</dt>
-            <dd class="col-sm-9">
-            ${pets[i].about_me}
-            </dd>`;
-            html += htmlSegment;
-       };
-       document.getElementById("pet_details_container").innerHTML = html;
-    } catch (error) {
-        console.error("Error while processing pet data:", error);
-    }
-}
+//             <dt class="col-sm-3">A Little Bit About Me:</dt>
+//             <dd class="col-sm-9">
+//             ${pets[i].about_me}
+//             </dd>`;
+//             html += htmlSegment;
+//        };
+//        document.getElementById("pet_details_container").innerHTML = html;
+//     } catch (error) {
+//         console.error("Error while processing pet data:", error);
+//     }
+// }
 
 document.addEventListener("DOMContentLoaded", function() {
     pet_card() });
