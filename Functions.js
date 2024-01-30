61330575743bba43080447eb9c83fb6d9898e9bc
Isha Nepal <ishanepal198@gmail.com>
2024-01-30 18:56:25 -0500

    Readme updated and indexing

--- Functions.js
+++ Functions.js
@@ -56,54 +56,50 @@ function getQueryParam(key){
 }
 
 function pet_details(id) {
-        if (id !== null && typeof pets !== 'undefined' && id >= 0 && id < pets.length) {
-            // const pet = pets[index];
-            let html = `
-            <div class="row">
-    <div class="col-lg-6 col-sm-12">
-        <img src="${pets[id].image}" class="card-img-top" alt="Image of a ${pets[id].breed} ${pets[id].type} on the grass">
-    </div>
-    <div class="col-lg-6 col-sm-12">
-        <dl class="row" id="pet_box">
-        </dl>
-    
-        <dt class="col-sm-3">Type:</dt>
-        <dd class="col-sm-9">${pets[id].type}</dd>
-        
-        <dt class="col-sm-3">Breed:</dt>
-        <dd class="col-sm-9">${pets[id].breed}</dd>
-        
-        <dt class="col-sm-3">Sex:</dt>
-        <dd class="col-sm-9">${pets[id].sex}</dd>
-        
-        <dt class="col-sm-3">Color:</dt>
-        <dd class="col-sm-9">${pets[id].color}</dd>
-        
-        <dt class="col-sm-3">Spayed / Neutered:</dt>
-        <dd class="col-sm-9">${pets[id].fixed}</dd>
+        if (id !== null && id >= 0 && id < pets.length) {
+            let html = 
+            `<div class="row">
+                <div class="col-lg-6 col-sm-12">
+                    <img src="${pets[id].image}" class="card-img-top" alt="Image of a ${pets[id].breed} ${pets[id].type} on the grass">
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <dl class="row" id="pet_box">
+                    </dl>
+                
+                    <dt class="col-sm-3">Type:</dt>
+                    <dd class="col-sm-9">${pets[id].type}</dd>
+                    
+                    <dt class="col-sm-3">Breed:</dt>
+                    <dd class="col-sm-9">${pets[id].breed}</dd>
+                    
+                    <dt class="col-sm-3">Sex:</dt>
+                    <dd class="col-sm-9">${pets[id].sex}</dd>
+                    
+                    <dt class="col-sm-3">Color:</dt>
+                    <dd class="col-sm-9">${pets[id].color}</dd>
+                    
+                    <dt class="col-sm-3">Spayed / Neutered:</dt>
+                    <dd class="col-sm-9">${pets[id].fixed}</dd>
 
-        <dt class="col-sm-3">Age:</dt>
-        <dd class="col-sm-9">${pets[id].age}</dd>
+                    <dt class="col-sm-3">Age:</dt>
+                    <dd class="col-sm-9">${pets[id].age}</dd>
 
-        <dt class="col-sm-3">Animal ID:</dt>
-        <dd class="col-sm-9">${pets[id].animal_id}</dd>
+                    <dt class="col-sm-3">Animal ID:</dt>
+                    <dd class="col-sm-9">${pets[id].animal_id}</dd>
 
-        <dt class="col-sm-3">Microchip Number:</dt>
-        <dd class="col-sm-9">${pets[id].microchip_num}</dd>
+                    <dt class="col-sm-3">Microchip Number:</dt>
+                    <dd class="col-sm-9">${pets[id].microchip_num}</dd>
 
-        <dt class="col-sm-3">Status:</dt>
-        <dd class="col-sm-9">${pets[id].status[0]}</dd>
-        <dd class="col-sm-9">${pets[id].status[1]}</dd>
-        <dd class="col-sm-9">${pets[id].status[2]}</dd>
-        <dd class="col-sm-9">${pets[id].status[3]}</dd>
+                    <dt class="col-sm-3">Status:</dt>
+                    <dd class="col-sm-9">${pets[id].status[0]}</dd>
+                    <dd class="col-sm-9">${pets[id].status[1]}</dd>
+                    <dd class="col-sm-9">${pets[id].status[2]}</dd>
+                    <dd class="col-sm-9">${pets[id].status[3]}</dd>
 
-        <dt class="col-sm-3">A Little Bit About Me:</dt>
-        <dd class="col-sm-9">
-        ${pets[id].about_me}
-        </dd>
-        </div>
-        </div>
-            `;
+                    <dt class="col-sm-3">A Little Bit About Me:</dt>
+                    <dd class="col-sm-9">${pets[id].about_me}</dd>
+                </div>
+            </div>`;
             document.getElementById('pet_details_container').innerHTML = html;
         } else {
            window.location.href = 'index.html';
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Contributors:
 - Julianna Truitt 
     - I worked on creating the array and helping members out when needed.
-- Isha Nepal
-    I worked on the creating the details.html page, added the icons for gender and ages from bootstrap and adding the details of the pets on the cards.
+- Isha Nepal 
+    - I worked on the details page functions and working out the final details.
 - Garrett Kinder
     - I worked on a lot of the functionality and creating the Functions sheet. I helped with editing the array for spelling and grammar. I aided in getting the pages to relate and pull from one another.
