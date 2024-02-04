ba350138dae6076fffdcd7bce9189924edc0aa41
juliannatruitt <truittj3@nku.edu>
2024-02-04 00:24:44 -0500

    adding in modal functionality. lets hope it works on yalls end as well

--- Functions.js
+++ Functions.js
@@ -28,23 +28,22 @@ async function pet_card() {
                 sex = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gender-male" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8"/></svg>`
             }
-            
+            // <button class="btn btn-primary" onclick="showModal(${i})">More information</button>
+            //<a href="detail.html?id=${i}" class="btn btn-primary">Details on ${pets[i].name}...</a>
             let htmlSegment = `
-                <div class="col-lg-4 col-sm-12">
+                <div class="col-lg-4 col-sm-12" data-index="${i}">
                     <div class="card" style="width: 18rem;">
                         <img src="${pets[i].image}" class="card-img-top" alt="Image of ${pets[i].name}">
                         <div class="card-body">
                             <h5 class="card-title">${pets[i].name}</h5>
-                            <div>${sex}${age}</div>
-                            <p class="card-text">${pets[i].name}, a ${pets[i].breed} ${pets[i].type}. ${pets[i].age} years old with ${pets[i].color} fur.</p>
-                            <a href="detail.html?id=${i}" class="btn btn-primary">Details on ${pets[i].name}...</a>
+                            <div>${sex}${age}</div><br />                                
                         </div>
                     </div>
                 </div>`;
                 html += htmlSegment;
         };
-
         document.getElementById("pet_container").innerHTML = html;
+
     } catch (error) {
         console.error("Error while processing pet data:", error);
     }
@@ -104,5 +103,37 @@ function pet_details(id) {
         } else {
            window.location.href = 'index.html';
         }
-    }
-;
+}
+
+function showModal(pet_index){
+    let info = 
+    `
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">${pets[pet_index].name}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <ul>
+                    <li><p class="card-text">${pets[pet_index].name}, a ${pets[pet_index].breed} ${pets[pet_index].type}. ${pets[pet_index].age} years old with 
+                    ${pets[pet_index].color} fur.</p></li>
+                    <li><p>Spayed/Neutered: ${pets[pet_index].fixed}</p></li>
+                    <li><p>Status: ${pets[pet_index].status}, </p></li>
+        
+                </ul>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary">
+                    <a href="detail.html?id=${pet_index}" style="color: white; text-decoration: none;">
+                        More information
+                    </a>
+                </button>
+                <button id="closeModal" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+    `;
+    document.getElementById('modal').innerHTML = info;
+}
+
